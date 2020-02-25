d35ec617868bfc872266de20757d3f5989ab2f14
Katsumasa Kitajima <k.k4.bky@gmail.com>
2020-02-25 21:47:49 +0900

    Reduced compilation warnings

--- include/my_utils_kk4.hpp
+++ include/my_utils_kk4.hpp
@@ -155,6 +155,8 @@ private:
     static struct timespec sub(const struct timespec& a, const struct timespec& b);
     static struct timespec add(const struct timespec& a, const struct timespec& b);
     static double toSec(const struct timespec& t);
+
+    bool measuring;
     
 #if defined(linux) || defined(__linux) || defined(__linux__)
     struct timespec time_start;
@@ -164,7 +166,7 @@ private:
     std::chrono::steady_clock::time_point time_start;
     std::chrono::nanoseconds offset;
 #endif
-    bool measuring;
+    
     struct timespec result;
     Mode mode;
 };
@@ -172,51 +174,6 @@ private:
 
 // sources
 
-//! 
-/*! 
-  Function to show a progress bar.
-  @param [in] progress Progress, must be within [0.0, 1.0].
-  @param [in] first_call Set true when call it for the first time.
-*/
-inline void progBarNh(const double progress, const bool first_call){
-
-    double prog;
-    if(progress < 0.0){
-        prog = 0.0;
-    }else if(progress > 1.0){
-        prog = 1.0;
-    }else{
-        prog = progress;
-    }
-
-    const char * nh[] = {"           ／⌒ヽ                  ",
-                         "⊂二二二（  ＾ω＾）二⊃           ",
-                         "                /             ﾌﾞｰﾝ ",
-                         "        （  ヽノ                   ",
-                         "         ﾉ>ノ                      ",
-                         "   三    レﾚ                       "};
-
-    
-    std::string offset((int)(prog * 40), ' ');
-
-    int prog_per = (int)(prog * 100);
-
-    if(! first_call){
-        std::cout << "\033[6A\r";
-    }
-
-    for(int i = 0; i < 6; i++){
-        std::cout << "|" << offset << nh[i];
-        if(i == 0){
-            std::cout << "\r\033[64C| " << prog_per << " %          \n";
-        }else{
-            std::cout << "\r\033[64C|           \n";
-        }
-    }
-
-    std::cout << std::flush;
-    return;
-}
 
 //! 
 /*! 
